232212cf15981cdd7d1b3483d0d1ffadcbfa40c8
VlaADanilov <helloampro@gmail.com>
2025-07-24 06:47:33 +0300

    Добавил бизнес логику для работы с заявками на блокирование карты

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -3,7 +3,6 @@ package com.example.bankcards.repository;
 import com.example.bankcards.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface CardRepository extends JpaRepository<Card, UUID> {
